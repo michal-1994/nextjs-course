0ef1bd33423b0c3a64d01ef3fee940cd96f8fd58
Michal <michalgrzegorczyk04@gmail.com>
2021-11-15 17:34:42 +0100

    Add meetups list

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_LIST = [
+    {
+        id: 'm1',
+        title: 'First meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Some address 5, 12345 Some City',
+        description: 'This is a first meetup',
+    },
+    {
+        id: 'm2',
+        title: 'Second meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+        address: 'Some address 10, 12345 Some City',
+        description: 'This is a second meetup',
+    },
+];
+
 function HomePage() {
     return (
-        <h1>The Home Page</h1>
+        <MeetupList meetups={DUMMY_LIST} />
     );
 }
 
